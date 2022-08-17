1f874dd5829bd079514d095267f27fd44748ae04
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-17 14:12:04 +0300

    Add Homework 025 & 027

---
+++ Homework027/Program.cs
@@ -0,0 +1,25 @@
+﻿// //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int a)
+{
+    int rem = 0;
+    while (a > 0)
+    {
+        rem = rem + a % 10;
+        a = a / 10;
+    }
+    return rem;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма цифр в числе {a} = {Sum(a)}");
+}
+
+PrintSum(digit);
+
+
+
+
---
+++ Homework029/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+
